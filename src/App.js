fba724ed8679b5373a1dea0caa099fd07071361d
pzync <shkhrgrv@gmail.com>
2018-10-09 12:02:32 +0530

    added conditional Favorites section based on Liked Food Recipes

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React, { Component } from "react";
 import "./App.css";
 import FoodItem from "./components/foodItem";
 
-const API_KEY = `01efc9c4b510d2af1a7cf4971f96559b`;
+const API_KEY = `22bb85986e161bafa350f51ba4c4c0a4`;
 
 class App extends Component {
   state = {
     foodItems: [],
-    hasLoaded: false
+    hasLoaded: false,
+    likeCount: 0
   };
 
   async componentDidMount() {
@@ -23,18 +24,48 @@ class App extends Component {
   }
 
   handleLike = foodItem => {
+    // to update the like status of a foodItem
     const foodItems = [...this.state.foodItems];
     const index = foodItems.indexOf(foodItem);
     foodItems[index] = { ...foodItem };
     foodItems[index].liked = !foodItems[index].liked;
-    this.setState({ foodItems });
+
+    // to update the likeCount of the list
+    const likeCount = foodItems.filter(f => f.liked).length;
+
+    // to update the state
+    this.setState({ foodItems, likeCount });
   };
 
   render() {
-    const { foodItems, hasLoaded } = this.state;
+    const { foodItems, hasLoaded, likeCount } = this.state;
     return (
       <div className="App">
         <h1>Your Recipes</h1>
+        <React.Fragment>
+          {likeCount ? (
+            <div>
+              <h2>
+                Favorites(
+                {likeCount})
+              </h2>
+              <div className="card-list fav-list">
+                {foodItems.filter(f => f.liked).map(f => (
+                  <FoodItem
+                    key={f.recipe_id}
+                    title={f.title}
+                    image={f.image_url}
+                    publisher={f.publisher}
+                    liked={f.liked}
+                    onLike={() => this.handleLike(f)}
+                  />
+                ))}
+              </div>
+              <hr />
+              <h2>All Recipes</h2>
+            </div>
+          ) : null}
+        </React.Fragment>
         <div className="card-list">
           {hasLoaded && foodItems.length
             ? foodItems.map(f => (
